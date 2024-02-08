22b17fc397977c288b84896d81fb55c52c5abba1
Michjadams <mjadams66@gmail.com>
2024-02-08 06:29:35 -0600

    wow, added a page to add cards manually so fast!

---
+++ scenes/add_single_word.gd
@@ -0,0 +1,11 @@
+extends Panel
+
+@onready var target_input = $target_input
+@onready var source_input = $source_input
+
+
+func _on_save_word_pressed():
+	var word_to_add = VocabularyWord.new(target_input.text, source_input.text)
+	VocabularyManager.add_words([word_to_add])
+	target_input.clear()
+	source_input.clear()
